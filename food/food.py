c4f46503eaa7d9e4350a3ff75b40813c799cfc69
roxana412 <roxana41200@gmail.com>
2017-08-07 15:03:13 -0400

    removing test

--- food/food.py
+++
@@ -1,8 +0,0 @@
-import pandas
-import matplotlib.pyplot as plt
-import food_access
-data = pandas.read_db("food/food_access.db")
-list_of_record = food_access.get_records()
-import matplotlib.pyplot as plt
-print(type(food/food_access.plot()))
-plt.show()
